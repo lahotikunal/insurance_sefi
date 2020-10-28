b21d4075b95347f9ecad06e3a145e83126fe1f55
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2020-10-28 17:04:04 +0530

    Update lucky7java.js

--- lucky7java.js
+++ lucky7java.js
@@ -103,7 +103,8 @@ rules.addEventListener('click', function() {
 });
 
 next_game.addEventListener('click',function(){
-  {location.href = "https://lahotikunal.github.io/lucky-7/";}
+  if(potValue<0)
+    {location.href = "https://lahotikunal.github.io/lucky-7/";}
 });
 //rules
 
